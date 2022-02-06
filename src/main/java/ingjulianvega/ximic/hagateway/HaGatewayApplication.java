56298dc720739d5fa892befafbd8254ac49b7dd5
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-02-05 20:20:27 -0500

    ✨ Initial add of project

---
+++ src/main/java/ingjulianvega/ximic/hagateway/HaGatewayApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.hagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaGatewayApplication.class, args);
+	}
+
+}
